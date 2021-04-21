37e06e21a82f54708f9900c4d8d9e4064c2d770f
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-04-21 17:25:10 +0300

    USER VIEW - Storage: Moved images to storage

--- storage/framework/views/ca1c4236bd7a52bc72db1c6695301de411ed5ccb.php
+++ storage/framework/views/ca1c4236bd7a52bc72db1c6695301de411ed5ccb.php
@@ -1,7 +1,5 @@
-<?php use Illuminate\Support\Arr;
-
-$__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('title', __('Not Found')); ?>
 <?php $__env->startSection('code', '404'); ?>
 <?php $__env->startSection('message', __('Not Found')); ?>
 
-<?php echo $__env->make('errors::minimal', Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/nabcellent-7/Desktop/PHP/My Projects/suf-laravel/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/nabcellent-7/Desktop/PHP/My Projects/suf-laravel/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
