0983c0b0abd1ea4d4e151da3ad40c62e83fcd6ed
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-04-23 05:55:01 +0300

    Major Changes

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -22,31 +22,7 @@ class AdminSeeder extends Seeder
         Admin::truncate();
 
         Admin::insert([
-            [
-                "first_name" => "Michael",
-                "last_name" => "Nabangi",
-                "username" => "lobengz",
-                "gender" => "Male",
-                "national_id" => 38907326,
-                "type" => 'Admin',
-                "email" => "michael.nabangi@strathmore.edu",
-                "password" => Hash::make("mike"),
-                "ip_address" => "127.0.0.1",
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now(),
-            ],[
-                "first_name" => "Michael",
-                "last_name" => "Nabangi",
-                "username" => "chapatisella",
-                "gender" => "Male",
-                "national_id" => 38907327,
-                "type" => 'Seller',
-                "email" => "miguelnabz@gmail.com",
-                "password" => Hash::make("mike"),
-                "ip_address" => "127.0.0.1",
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now(),
-            ]
+
         ]);
     }
 }
